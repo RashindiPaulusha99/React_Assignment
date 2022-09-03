8bf136f27a2bf9e988191a1833fed85a9a00364c
RashindiPaulusha99 <rashindipaulusha837@gmail.com>
2022-09-03 17:41:31 +0530

    add dashboard page

--- src/pages/sessions/DashBoard/index.jsx
+++ src/pages/sessions/DashBoard/index.jsx
@@ -4,82 +4,19 @@ import {styleSheet} from "./style";
 import Grid from '@mui/material/Grid';
 import {Link} from "react-router-dom";
 import GDSEButton from "../../../components/Home/Common/Button";
-import { ValidatorForm, TextValidator} from 'react-material-ui-form-validator';
-import GDSEDataTable from "../../../components/Home/Common/Table";
+import Divider from '@mui/material/Divider';
+import Typography from '@mui/material/Typography';
 
 class DashBoard extends Component {
 
     constructor(props) {
         super(props);
-
-        this.state = {
-            formData:{
-                name:"",
-                gender:"",
-                nic:"",
-                email:""
-            },
-
-            data:[],
-
-            loaded: false,
-
-            columns: [
-                {
-                    field: 'name',
-                    headerName: 'Customer Name',
-                    width: 350
-                },
-                {
-                    field: 'gender',
-                    headerName: 'Gender',
-                    width: 350
-                },
-                {
-                    field: 'nic',
-                    headerName: 'NIC',
-                    width: 350
-                },
-                {
-                    field: 'email',
-                    headerName: 'Email',
-                    width: 350
-                }
-            ]
-
-        }
-    }
-
-    clearFields = ()=>{
-        this.setState({
-            formData:{
-                name:"",
-                gender:"",
-                nic:"",
-                email:""
-            }
-        })
-    }
-
-    loadData =()=>{
-        let formData = this.state.formData
-        this.setState({
-            loaded:true,
-            data:formData
-        })
-        console.log(this.state.formData)
-        console.log(this.state.data)
     }
 
     render() {
         let { classes } = this.props
         return (
             <Fragment>
-                <ValidatorForm
-                    ref="form"
-                    onSubmit={this.loadData}
-                    onError={errors => console.log(errors)}
-                >
                     <Grid container spacing="12">
                         <div className={classes.title__container}>
                             <Grid item lg={4} md={4} sm={4} xm={4} style={{paddingLeft: '3%', paddingTop: '2%',paddingBottom: '1%'}}>
@@ -102,82 +39,35 @@ class DashBoard extends Component {
                                 </div>
                             </Grid>
                         </div>
-                        <Grid item lg={6} md={6} sm={6} xm={6} style={{paddingLeft: '5%',paddingTop: '2%'}}>
-                            <TextValidator id="outlined-basic" label="Customer Name" variant="outlined" size="small"
-                                       style={{width: '90%'}}
-                                           validators={['required','isString']}
-                                           value={this.state.formData.name}
-                                           onChange={(e)=>{
-                                               let data=this.state.formData
-                                               data.name=e.target.value
-                                               this.setState(data);
-                                           }}
-                            />
-                        </Grid>
-                        <Grid item lg={6} md={6} sm={6} xm={6} style={{paddingTop: '2%'}}>
-                            <TextValidator id="outlined-basic" label="Gender" variant="outlined" size="small"
-                                       style={{width: '90%'}}
-                                           validators={['required','isString']}
-                                           value={this.state.formData.gender}
-                                           onChange={(e)=>{
-                                               let data=this.state.formData
-                                               data.gender=e.target.value
-                                               this.setState(data);
-                                           }}
-                            />
-                        </Grid>
-                        <Grid item lg={6} md={6} sm={6} xm={6} style={{paddingLeft: '5%',paddingTop: '2%'}}>
-                            <TextValidator id="outlined-basic" label="Email" variant="outlined" size="small"
-                                       style={{width: '90%'}}
-                                           validators={['required','isEmail']}
-                                           value={this.state.formData.email}
-                                           onChange={(e)=>{
-                                               let data=this.state.formData
-                                               data.email=e.target.value
-                                               this.setState(data);
-                                           }}
-                            />
-                        </Grid>
-                        <Grid item lg={6} md={6} sm={6} xm={6} style={{paddingTop: '2%'}}>
-                            <TextValidator id="outlined-basic" label="NIC" variant="outlined" size="small"
-                                       style={{width: '90%'}}
-                                           validators={['required','isString']}
-                                           value={this.state.formData.nic}
-                                           onChange={(e)=>{
-                                               let data=this.state.formData
-                                               data.nic=e.target.value
-                                               this.setState(data);
-                                           }}
-                            />
-                        </Grid>
-                        <Grid item lg={12} md={12} sm={12} xm={12}
-                              style={{display: 'flex', justifyContent: 'flex-end', paddingRight: '5%'}}>
-                            <div style={{paddingRight: '1%'}}>
-                                <GDSEButton variant="contained" label="Clear" color='error' onClick={this.clearFields}/>
-                            </div>
-                            <div>
-                                <GDSEButton variant="contained" label="Save" type="submit"
-                                            onClick={()=>{
-                                                this.loadData()
-                                            }}/>
-                            </div>
-                        </Grid>
-                        <Grid item lg={12} md={12} sm={12} xm={12}>
 
+                        <Grid className={classes.grid__container}>
+                            <Grid item lg={6} md={6} sm={6} xm={6} style={{paddingLeft: '20%',paddingRight: '5%'}}>
+                                <div className={classes.div}>
+                                    <div style={{width:'100%',height:'50%'}}>
+                                        <Typography variant="h2" gutterBottom  style={{textAlign:'center'}}>
+                                        Customers
+                                    </Typography>
+                                    <Typography variant="h2" gutterBottom style={{textAlign:'center'}}>
+                                        100
+                                    </Typography>
+                                    </div>
+                                </div>
+                            </Grid>
+
+                            <Grid item lg={6} md={6} sm={6} xm={6} style={{paddingLeft: '20%',paddingRight: '5%'}}>
+                                <div className={classes.div}>
+                                    <div style={{width:'100%',height:'50%'}}>
+                                        <Typography variant="h2" gutterBottom style={{textAlign:'center'}}>
+                                            Items
+                                        </Typography>
+                                        <Typography variant="h2" gutterBottom style={{textAlign:'center'}}>
+                                            100
+                                        </Typography>
+                                    </div>
+                                </div>
+                            </Grid>
                         </Grid>
                     </Grid>
-                </ValidatorForm>
-                <Grid container style={{ height: 400, width: '100%', marginTop: '50px'}}>
-                    <GDSEDataTable
-                        stickyHeader aria-label="sticky table"
-                        columns={this.state.columns}
-                        rows={this.state.data}
-                        rowsPerPageOptions={4}
-                        pageSize={4}
-                        checkboxSelection={true}
-                    />
-                </Grid>
-
             </Fragment>
         )
     }
---
+++ src/pages/sessions/DashBoard/style.js
@@ -0,0 +1,25 @@
+export const styleSheet = {
+    title__container: {
+        display: 'flex',
+        height: '20%',
+        width: '100%',
+        backgroundColor: 'black'
+    },
+    grid__container: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100%',
+        width: '80%',
+        paddingTop: '15%'
+    },
+    div: {
+        display: 'flex',
+        height: '100%',
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        border:'1px solid black',
+        paddingTop: '20%'
+    }
+}
